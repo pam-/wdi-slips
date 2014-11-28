f6bdd7de0b7ba2bac853ccc72039de8b8653be6c
pam- <pam.assogba@gmail.com>
2014-11-27 23:22:32 -0500

    chose questions for for monday slips

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,5 +1,21 @@
-function start() {
-	console.log('here')
+//data modeling
+function Slip(question) {
+	// this.student = student
+	this.question = question
 }
 
-window.onload = start;
+function newSlip(){
+	// console.log(questions)
+	return new Slip(questions[0].cli[0])
+	questions[0].cli.splice(0, 1)
+}
+
+function start() {
+	$('button').on('click', function(){
+		slip = newSlip();
+		console.log(slip.question)
+		console.log(questions[0].cli)
+	})
+};
+
+$(document).ready(start);
