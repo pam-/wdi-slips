17515e0295a05af109d12b435b94e9c84b703e1a
pam- <pam.assogba@gmail.com>
2014-11-28 15:30:45 -0500

    migrated data

--- db/migrate/20141128202245_create_questions.rb
+++ db/migrate/20141128202245_create_questions.rb
@@ -2,6 +2,8 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
     	t.string :content
+    	t.references :topic
+    	t.timestamps
     end
   end
 end
