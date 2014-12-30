f5a096064fa65df7add498adf32eb3bee51e7e9a
pam- <pam.assogba@gmail.com>
2014-12-29 19:44:11 -0500

    styled user show page, missing footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
-		@questions = current_user.questions.paginate(page: params[:page], per_page: 4)
-		@question = Question.new
+		@questions = @user.questions.paginate(page: params[:question], per_page: 4)
+		@answers = @user.answers.paginate(page: params[:answer], per_page: 4)
 		respond_to do |format|
 			format.html
-			format.json { render json: @question }
+			format.json { render json: @questions }
 		end
 	end
 end 
